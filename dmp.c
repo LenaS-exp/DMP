47b2119d2e1fbc24c692dadfddf280d1fdf954f1
LenaS-exp <74903006+LenaS-exp@users.noreply.github.com>
2024-05-22 17:59:41 +0300

    cosmetic changes dmp.c

--- dmp.c
+++ dmp.c
@@ -14,7 +14,7 @@ struct my_dm_target {
 };
 
 struct stats {
-    unsigned long long read_reqs, read_size , write_reqs, write_size;
+	unsigned long long read_reqs, read_size , write_reqs, write_size;
 };
 
 static struct stats stat = { 0, 0, 0, 0 };
@@ -37,9 +37,16 @@ static ssize_t stat_show(struct kobject *kobj, struct kobj_attribute *attr, char
 		avg_write = stat.write_size / stat.write_reqs;
 	}
 
-	return
-	sprintf(buf,"\nread_reqs: %llu \naverage read_size: %llu \nwrite_reqs: %llu \naverage write_size: %llu \ntotal_reqs: %llu \naverage block size: %llu \n\n",
-	stat.read_reqs, avg_read, stat.write_reqs, avg_write, stat.read_reqs + stat.write_reqs, (avg_read + avg_write) / 2);
+	return sprintf(buf,"\n\
+		read_reqs: %llu \n\
+		average read_size: %llu\n\
+		write_reqs: %llu \n\
+		average write_size: %llu \n\
+		total_reqs: %llu \n\
+		average block size: %llu \n\n",
+		stat.read_reqs, avg_read,
+		stat.write_reqs, avg_write,
+		stat.read_reqs + stat.write_reqs, (avg_read + avg_write) / 2);
 }
 
 
@@ -56,7 +63,7 @@ static struct kobj_attribute stat_attr = __ATTR(volumes, 0440, stat_show, stat_s
 /*Constructor function*/
 static int dmp_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 {
-    struct my_dm_target *mdt;
+    	struct my_dm_target *mdt;
 	unsigned long long start;
 
 	if (argc != 2) {
@@ -99,9 +106,9 @@ bad:
 /*Destruction function*/
 static void dmp_dtr(struct dm_target *ti)
 {
-    struct my_dm_target *mdt = (struct my_dm_target *) ti->private;
+	struct my_dm_target *mdt = (struct my_dm_target *) ti->private;
 	dm_put_device(ti, mdt->dev);
- 	kfree(mdt);
+	kfree(mdt);
 }
 
 
@@ -109,72 +116,72 @@ static void dmp_dtr(struct dm_target *ti)
 static int dmp_map(struct dm_target *ti, struct bio *bio)
 {
     
-    struct my_dm_target *mdt = (struct my_dm_target *) ti->private;
-
+	struct my_dm_target *mdt = (struct my_dm_target *) ti->private;
+	
 	bio_set_dev(bio, mdt->dev->bdev);
-
+	
 	/*bio_data_dir(bio) returns the data direction, READ or WRITE.*/
-
+	
 	if (bio_data_dir(bio) == READ) {
-        stat.read_reqs++; //read request count
-        stat.read_size += bio->bi_iter.bi_size; //read block size count
-    } else {
-        stat.write_reqs++; //write request count
-        stat.write_size += bio->bi_iter.bi_size; //write block size count
-    }
-        
+		stat.read_reqs++; //read request count
+		stat.read_size += bio->bi_iter.bi_size; //read block size count
+	} else {
+		stat.write_reqs++; //write request count
+		stat.write_size += bio->bi_iter.bi_size; //write block size count
+	}
+	
 	submit_bio(bio);
-
+	
 	return DM_MAPIO_SUBMITTED;
 
 }
 
 
 static struct target_type dmp_target = {
-    .name = DMP_NAME,
-    .version = {1, 0, 0},
-    .module = THIS_MODULE,
-    .ctr = dmp_ctr,
-    .dtr = dmp_dtr,
-    .map = dmp_map,
+	.name = DMP_NAME,
+	.version = {1, 0, 0},
+	.module = THIS_MODULE,
+	.ctr = dmp_ctr,
+	.dtr = dmp_dtr,
+	.map = dmp_map,
 };
 
 
 static int dmp_init(void)
 {
-    int r;
-
-    r = dm_register_target(&dmp_target);
-    if (r < 0) {
-        printk(KERN_ERR "Failed to register device mapper target\n");
-        return r;
-    }
-
+	int r;
+	
+	r = dm_register_target(&dmp_target);
+	if (r < 0) {
+		printk(KERN_ERR "Failed to register device mapper target\n");
+		return r;
+	}
+	
 	/*Creating a directory in /sys/kernel/ */
-    kob = kobject_create_and_add("stat", &THIS_MODULE->mkobj.kobj);
-    if (!kob) {
+	kob = kobject_create_and_add("stat", &THIS_MODULE->mkobj.kobj);
+	if (!kob) {
 		return -ENOMEM;
 	}
-
-    r = sysfs_create_file(kob, &stat_attr.attr);
-    if (r) {
-        printk(KERN_ERR "Failed to create sysfs file for statistics\n");
-    } else {
-        printk(KERN_INFO "Device Mapper Proxy initialized\n");
-    }
-
-    return 0;
+	
+	r = sysfs_create_file(kob, &stat_attr.attr);
+	if (r) {
+		printk(KERN_ERR "Failed to create sysfs file for statistics\n");
+	} else {
+		printk(KERN_INFO "Device Mapper Proxy initialized\n");
+	}
+	
+	return 0;
 }
 
 
 static void dmp_exit(void)
 {  
 	/*Freeing Kobj*/
-    kobject_put(kob);
-
-    dm_unregister_target(&dmp_target);
-    sysfs_remove_file(kob, &stat_attr.attr);
-    printk(KERN_INFO "Device Mapper Proxy unloaded\n");
+	kobject_put(kob);
+	
+	dm_unregister_target(&dmp_target);
+	sysfs_remove_file(kob, &stat_attr.attr);
+	printk(KERN_INFO "Device Mapper Proxy unloaded\n");
 }
 
 
